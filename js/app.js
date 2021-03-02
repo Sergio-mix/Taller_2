bf414761dad1a81235f45a8018b1bfa159b111a3
SergioHZ <alejo.80.123@hotmail.com>
2021-03-02 07:21:12 -0500

    Cambio_1_Martes_Rgistrar

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
+//Selector
 addEventListener("DOMContentLoaded", () => {
   const btn_menu = document.querySelector('.btn_menu');
   if(btn_menu){
       btn_menu.addEventListener('click', () =>{
         const menu_items = document.querySelector('.menu_items');
         menu_items.classList.toggle('show')
-
       })
   }
 });
+
+
+//modal
+const open = document.getElementById('boton-ubicacion');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('cancelar');
+
+open.addEventListener('click', () => {
+  modal_container.classList.add('show');  
+});
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
+
+  
