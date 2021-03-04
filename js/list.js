cf3490525e8fb271432388ee1ea2bb44e11db902
Pc <bmorenor@unbosque.edu.co>
2021-03-03 21:44:43 -0500

    busqueda 1 /3 en proceso

--- js/list.js
+++ js/list.js
@@ -1,42 +1,62 @@
-class DataTable{
-    element;
-    headers;
-    items;
-    copyItems;
-    selected;
-    pagination;
-    numberOfEntries;
-    headersButtons;
+class DataTable {
+  element;
+  headers;
+  items;
+  copyItems;
+  selected;
+  pagination;
+  numberOfEntries;
+  headersButtons;
 
-    constructor(selector, headersButtons){
+  constructor(selector, headersButtons) {
+    this.element = document.querySelector(selector);
 
-        this.element = document.querySelector(selector);
+    this.headers = [];
+    this.items = [];
+    this.pagination = {
+      total: 0,
+      noItemsperpage: 0,
+      noPages: 0,
+      actual: 0,
+      pointer: 0,
+      diff: 0,
+      lastPageBeforeDots: 0,
+      noButtonsBeforeDots: 4,
+    };
 
-        this.headers = [];
-        this.items = [];
-        this.pagination ={ total:0,
-            noItemsperpage: 0,
-            noPages: 0,
-            actual: 0,
-            pointer: 0,
-            diff: 0,
-            lastPageBeforeDots: 0,
-            noButtonsBeforeDots: 4
-        };
+    this.selected = [];
+    this.numberOfEntries = 5;
+    this.headersButtons = headersButtons;
+  }
+  parse() {
+    const headers = [...this.element.querySelector("thead tr").children];
+    const trs = [...this.element.querySelector("tbody").children];
 
-        this.selected = [];
-        this.numberOfEntries = 5;
-        this.headersButtons = headersButtons;
+    headers.forEach((element) => {
+      this.headers.push(element.textContent);
+    });
+    trs.forEach((tr) => {
+      const cells = [...tr.children];
 
-    }
-    parse(){
-        const headers = [...this.element.querySelector('thead tr').children];
-        const trs = [...this.element.querySelector('tbody').children];
-        
-        headers.forEach(element=>{
-            this.headers.push(element.textContent);
-        });
-            console.log(this.headers);
-     
-    }
+      const item = {
+        id: this.generateUUID(),
+        values: [],
+      };
+      cells.forEach((cell) => {
+        if (cell.children.length > 0) {
+          // const status = [...cells.children][0].getAtrribute('class');
+          const searchElement = [...cell.children][0];
+          const search = searchElement.getAttribute("class");
+  
+            item.values.push(cell.textContent);
+          
+        }
+      });
+      this.items.push(item);
+    });
+    console.log(this.items);
+  }
+  generateUUID() {
+    return (Date.now() * Math.floor(Math.random() * 100000)).toString();
+  }
 }
