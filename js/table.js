81278d787647790069eee8810804dbc1fc576272
Pc <bmorenor@unbosque.edu.co>
2021-03-09 19:11:52 -0500

    Cambio tabla

--- js/table.js
+++ js/table.js
@@ -9,6 +9,9 @@ $(document).ready(function () {
             { "data": "size" },
             { "data": "potentDangerous" },
             { "data": "neighborhood" },
+            {"defaultContent":""},
+            {"defaultContent":""},
+            {"defaultContent":""},
             { "defaultContent": "<button type='button' class='edit btn btn-primary'><i class='fa fa-pencil-square-o'></i></button>	<button type='button' class='eliminar btn btn-danger' data-toggle='modal' data-target='#modalEliminar' ><i class='fa fa-trash-o'></i></button>" }
         ]
     });
